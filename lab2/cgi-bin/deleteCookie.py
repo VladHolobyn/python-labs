b6a0132daf639e1346fbe41842a09611f67eac5a
VladHolobyn <vlad.holobyn@gmail.com>
2023-10-11 23:57:28 +0300

    Task 2: Added cookie

---
+++ lab2/cgi-bin/deleteCookie.py
@@ -0,0 +1,14 @@
+print(f"Set-cookie: counter={0}; Expires=Thu, 01 Jan 1970 00:00:00 GMT")
+
+print(f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Cookie</title>
+</head>
+<body>
+    <h1>Cookie successfully deleted!</h1>
+</body>
+</html>
+""")
--- lab2/cgi-bin/form.py
+++ lab2/cgi-bin/form.py
@@ -1,16 +1,13 @@
 import cgi
 import html
+import http.cookies
+import os
 
 form = cgi.FieldStorage()
 
-
-username = form.getfirst("name", "undefined")
-age = form.getfirst("age", "undefined")
-
-username = html.escape(username)
-age = html.escape(age)
-
-doSport = form.getvalue("doSport", "undefined")
+username = html.escape( form.getfirst("name", "Not specified") )
+age = html.escape(form.getfirst("age", "Not specified"))
+doSport = form.getvalue("doSport", "Not specified")
 
 genres = ["Novel", "Fantasy", "Detective", "Nonfiction"]
 checkedGenres = []
@@ -20,18 +17,27 @@ for group in genres:
         checkedGenres.append(group)
 
 
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+counterCookie = int(cookie.get("counter").value) + 1 if "counter" in cookie else 1
+
+print(f"Set-cookie: counter={counterCookie};")
+print("Content-type:text/html\r\n\r\n")
+
 print(f"""
 <!DOCTYPE html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Обробка форми</title>
+    <title>Form</title>
 </head>
 <body>
     <p>Your name: {username}</p>
     <p>Your age: {age}</p>
     <p>Do you do sports? - {doSport}</p>
     <p>Your favorite genres of books: {", ".join(checkedGenres)}</p>
+    
+    <p>Counter: {counterCookie}</p>
+
 </body>
 </html>
 """)
