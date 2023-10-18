7068142533f79e8a9338d79d2cc689ad53e283a5
VladHolobyn <vlad.holobyn@gmail.com>
2023-10-18 11:38:08 +0300

    LAB-3: Added pages and routes

--- lab3/app.py
+++ lab3/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 
 if __name__ == '__main__':
