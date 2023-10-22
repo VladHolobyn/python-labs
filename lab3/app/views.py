ac3b87b11f9a4b1d00eaa082bd6a36ec20646067
VladHolobyn <vlad.holobyn@gmail.com>
2023-10-22 13:09:24 +0300

    [LAB-4] Task 2: Implemented login functionality

--- lab3/app/views.py
+++ lab3/app/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, make_response, session
 from datetime import datetime
 import os
 from app import app
+import json
 
 
 skills = ["java", "postgres", "spring", "hibernate", "junit", "docker"]
@@ -26,8 +27,25 @@ def contacts_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', os=os.name, user_agent=request.headers.get('User-Agent'), time=datetime.now())
+    if request.method == 'POST':
+        username = request.form.get("name")
+        password = request.form.get("password")
+
+        json_url = os.path.join(os.path.realpath(os.path.dirname(__file__)), "static/data", "login.json")
+        with open(json_url) as f:
+            users = json.load(f).get("users")
+            if any(user["name"] == username and user["password"] == password for user in users): 
+                session["username"] = username
+                return redirect(url_for("info_page"))
+            else:
+                session["error_message"] = "Wrong data! Try again!"
+                return redirect(url_for("login"))
+    
+    if session.get("username"):
+        return redirect(url_for("info_page"))   
+
+    return render_template('login.html', message=session.pop("error_message", None), os=os.name, user_agent=request.headers.get('User-Agent'), time=datetime.now())
 
 @app.route('/info')
 def info_page():
-    return render_template('info.html', os=os.name, user_agent=request.headers.get('User-Agent'), time=datetime.now())
+    return render_template('info.html', username=session.get("username"), os=os.name, user_agent=request.headers.get('User-Agent'), time=datetime.now())
