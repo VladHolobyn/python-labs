829daf1004512133a7a1d8e7853d98f737f40952
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-02 07:54:27 +0200

    [LAB-6] Added SQLite to the project

--- lab3/app/__init__.py
+++ lab3/app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fsCdvb3kb2Dep1gfdkFSD2Fw'
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
 
 from app import views
