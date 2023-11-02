f6cf53ee2dc03026e2e47dca77cefdfeb32ce3ad
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-02 08:11:15 +0200

    [LAB-6] Added model

--- lab3/app/__init__.py
+++ lab3/app/__init__.py
@@ -2,14 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
+class Todo(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(100))
+    complete: Mapped[bool] = mapped_column(Boolean)
+    
 
 with app.app_context():
     db.create_all()
