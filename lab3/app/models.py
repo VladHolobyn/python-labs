1bd18aa928a27885feb9018bdfafeb2a3ad40413
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-02 11:36:38 +0200

    [TEST-1] Added Feedback table

--- lab3/app/models.py
+++ lab3/app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Integer, String, Boolean, Date
+from sqlalchemy import Integer, String, Boolean, Date, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
-from datetime import date
+from datetime import date, datetime
 from app import db
 
 class Todo(db.Model):
@@ -8,4 +8,9 @@ class Todo(db.Model):
     title: Mapped[str] = mapped_column(String(100))
     due_date: Mapped[date] = mapped_column(Date) 
     complete: Mapped[bool] = mapped_column(Boolean)
-    
+
+class Feedback(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_email: Mapped[str] = mapped_column(String)
+    text: Mapped[str] = mapped_column(String)
+    date: Mapped[datetime] = mapped_column(DateTime) 
