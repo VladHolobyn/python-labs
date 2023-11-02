9988a8fd3a03895fa943c9b48a4e7e6a5541f757
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-02 09:02:53 +0200

    [LAB-6] Added new column to db

--- lab3/app/__init__.py
+++ lab3/app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import Integer, String, Boolean, Date
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 class Todo(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
+    due_date: Mapped[date] = mapped_column(Date) 
     complete: Mapped[bool] = mapped_column(Boolean)
     
 
