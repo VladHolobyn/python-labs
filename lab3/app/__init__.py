4d214b083c614fabdea506d50d5134723a012316
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-02 08:01:36 +0200

    [LAB-6] Added migrations

--- lab3/app/__init__.py
+++ lab3/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -7,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-db.init_app(app)
+migrate = Migrate(app, db)
 
 
 with app.app_context():
