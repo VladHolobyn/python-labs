24b494eb0b50669e7618ff02a996a9974e319c8b
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-08 12:52:58 +0200

    [LAB-8] Added flask-login to the project

--- lab3/app/__init__.py
+++ lab3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,5 +10,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app import views
