1fd9eceebadeedaa1fe9e2a41ac41f942c583e5a
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-16 20:06:00 +0200

    [LAB-10] Added category

--- lab3/app/todo/models.py
+++ lab3/app/todo/models.py
@@ -8,3 +8,9 @@ class Todo(db.Model):
     title: Mapped[str] = mapped_column(String(100))
     due_date: Mapped[date] = mapped_column(Date, nullable=True) 
     complete: Mapped[bool] = mapped_column(Boolean)
+    category_id: Mapped[int] = mapped_column(Integer, db.ForeignKey('category.id', name='fk_category'), nullable=True)
+
+class Category(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    todos = db.relationship('Todo', backref='category')
