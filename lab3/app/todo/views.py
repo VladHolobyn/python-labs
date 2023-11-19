f88afde0068fba7968de0286253d7703edc2edbe
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-19 14:22:11 +0200

    [LAB-10] Some refactoring in views

--- lab3/app/todo/views.py
+++ lab3/app/todo/views.py
@@ -5,41 +5,57 @@ from .models import Todo, Category
 from . import todo_bp
 
 
-@todo_bp.route('/')
+@todo_bp.route('/', methods=["GET"])
 def todo_page():
     form=TodoForm()
     form.categories.choices = [(c.id, c.name) for c in Category.query.all()] 
     return render_template("todo/todo.html", todo_list=Todo.query.all(), form=form)
  
-@todo_bp.route("/add", methods=["POST"])
+@todo_bp.route("/", methods=["POST"])
 def add():
     form=TodoForm()
-    print(form.categories.data)
+
     new_todo = Todo(
-        title=form.title.data, 
-        due_date=form.due_date.data, 
-        category_id=form.categories.data,
-        complete=False
+        title = form.title.data, 
+        due_date = form.due_date.data, 
+        category_id = form.categories.data,
+        complete = False
     )
-    db.session.add(new_todo)
-    db.session.commit()
+    try: 
+        db.session.add(new_todo)
+        db.session.commit()
+        flash('Todo added!', category='success')
+    except:
+        db.session.rollback()
+        flash('Error!', category='danger')
     return redirect(url_for("todo.todo_page"))
  
 @todo_bp.route("/update/<int:id>")
 def update(id):
     todo = Todo.query.get_or_404(id)
     todo.complete = not todo.complete   
-    db.session.commit()
+    try: 
+        db.session.commit()
+        flash(f'Todo({todo.id}) updated!', category='success')
+    except:
+        db.session.rollback()
+        flash('Error!', category='danger')    
     return redirect(url_for("todo.todo_page"))
  
 @todo_bp.route("/delete/<int:id>")
 def delete(id):
     todo = Todo.query.get_or_404(id)
-    db.session.delete(todo)
-    db.session.commit()
+    try: 
+        db.session.delete(todo)
+        db.session.commit()
+        flash(f'Todo({todo.id}) deleted!', category='success')
+    except:
+        db.session.rollback()
+        flash('Error!', category='danger')    
     return redirect(url_for("todo.todo_page"))
 
-@todo_bp.route('/categories')
+
+@todo_bp.route('/categories', methods=["GET"])
 def category_page():
     return render_template("todo/categories.html", categories=Category.query.all(), form=CategoryForm())
 
@@ -48,11 +64,11 @@ def add_category():
     form=CategoryForm()
     
     if form.validate_on_submit():
+        new_category = Category(name = form.name.data)
         try:
-            new_category = Category(name=form.name.data)
             db.session.add(new_category)
             db.session.commit()
-            flash('Category created!', category='success')
+            flash(f'Category({new_category.name}) created!', category='success')
         except:
             flash('Error!', category='danger')
             db.session.rollback()
@@ -67,7 +83,7 @@ def delete_category(id):
     try:
         db.session.delete(category)
         db.session.commit()
-        flash('Category deleted!', category='success')
+        flash(f'Category({category.id}) deleted!', category='success')
     except:
         flash('Error!', category='danger')
         db.session.rollback()
