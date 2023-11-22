c39188310a0c9dda4645c15ea248044d3e16738c
VladHolobyn <vlad.holobyn@gmail.com>
2023-11-22 16:06:37 +0200

    [LAB-11] Added post model and post blueprint

---
+++ lab3/app/posts/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Integer, String, Boolean, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date, datetime
+from ..extensions import db
+import enum
+
+class EnumPriority(enum.Enum):
+    low = 1 
+    medium = 2 
+    high = 3
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    text: Mapped[str] = mapped_column(String)
+    image: Mapped[str] = mapped_column(String, nullable=True)
+    created: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.now().timestamp())
+    type: Mapped[EnumPriority] = mapped_column(db.Enum(EnumPriority), default='low') 
+    enabled: Mapped[bool] = mapped_column(Boolean, default=False)
+    user_id: Mapped[int] = mapped_column(Integer, db.ForeignKey('user.id', name='fk_user'))
+
+    def __repr__(self) -> str:
+        return f"ID:{self.id} Title:{self.title} Created:{self.created} UserID: {self.user_id}"
+
+# class Category(db.Model):
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+#     name: Mapped[str] = mapped_column(String)
+#     todos = db.relationship('Todo', backref='category')
