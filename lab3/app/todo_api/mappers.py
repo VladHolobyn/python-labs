9a92500a676605a8a23eb2d6875fcd99f4779807
VladHolobyn <vlad.holobyn@gmail.com>
2023-12-07 14:01:53 +0200

    [LAB-13] Added update endpoint

--- lab3/app/todo_api/mappers.py
+++ lab3/app/todo_api/mappers.py
@@ -19,3 +19,23 @@ class TodoMapper():
             category_id = dto.get('category_id'),
             complete = dto.get('complete', False)
         )
+    
+    def updateFromDto(todo, dto):
+        title = dto.get('title')
+        due_date = dto.get('due_date')
+        complete = dto.get('complete')
+        category_id = dto.get('category_id')
+
+        if title:
+            todo.title = title
+
+        if due_date:
+            todo.due_date = datetime.strptime(due_date, "%Y-%m-%d").date()
+
+        if complete:
+            todo.complete = complete
+
+        if category_id:
+            todo.category_id = category_id
+
+        return todo
--- lab3/app/todo_api/views.py
+++ lab3/app/todo_api/views.py
@@ -48,4 +48,28 @@ def create_todo():
         return jsonify({"message": "Error"}), 500
 
 
+@todo_api_bp.route('/<int:id>', methods=["PUT"])
+def update_todo(id):  
 
+    data = request.get_json()
+    if not data:
+        return jsonify({"message": "No data provided"}), 400
+    
+    todo = Todo.query.filter_by(id=id).first()
+    if not todo:
+        return jsonify({"message": f"Todo with id:{id} not found"}), 404
+    
+    isValid, response = TodoValidator.validateForUpdate(data)
+    if not isValid:
+        return jsonify({'message': response['message']}),  response['status_code']
+
+    TodoMapper.updateFromDto(todo, data)
+
+    try: 
+        db.session.commit()
+        new_todo = Todo.query.filter_by(id=id).first()
+        return jsonify(TodoMapper.toDto(new_todo)), 200
+    except:
+        db.session.rollback()
+        return jsonify({"message": "Error"}), 500
+ 
