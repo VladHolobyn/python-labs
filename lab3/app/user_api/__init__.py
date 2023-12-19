1ba7e1cb42a9351f51a7bd698301d26cbf637aa7
VladHolobyn <vlad.holobyn@gmail.com>
2023-12-19 13:12:24 +0200

    [LAB-15] Fixed error handler

--- lab3/app/user_api/__init__.py
+++ lab3/app/user_api/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask_restful import Api
+from marshmallow import ValidationError
 from .views import UserApi, UsersApi
 
 user_api_bp = Blueprint('user_api', __name__)
@@ -7,3 +8,7 @@ api = Api(user_api_bp)
 
 api.add_resource(UsersApi, '/users')
 api.add_resource(UserApi, '/users/<int:id>')
+
+@user_api_bp.errorhandler(ValidationError)
+def handle_marshmallow_error(e):
+    return jsonify(e.messages), 400
