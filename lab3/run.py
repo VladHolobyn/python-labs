0641913d637f6484f71eca84df5bc2b38a1dd00e
VladHolobyn <vlad.holobyn@gmail.com>
2023-12-21 12:41:08 +0200

    Added gunicorn

--- lab3/run.py
+++ lab3/run.py
@@ -1,7 +1,5 @@
 import os
 from app import create_app
 
-
-if __name__ == '__main__':
-    config_name = os.environ.get('CONFIG','default')
-    create_app(config_name).run()
+config_name = os.environ.get('CONFIG','default')
+app = create_app(config_name).run()
